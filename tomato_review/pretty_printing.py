98eed4860935e70af413ada779a10371435837ee
SushiNinja <jin.huang2@h-partners.com>
2026-02-05 16:53:22 +0000

    style: remove padding spaces

--- tomato_review/pretty_printing.py
+++ tomato_review/pretty_printing.py
@@ -21,13 +21,13 @@ CEND = "\033[0m"
 console = Console()
 
 TOMATO_ART = r"""
-  ,d                                               ,d                
-  88                                               88                
-MM88MMM ,adPPYba,  88,dPYba,,adPYba,  ,adPPYYba, MM88MMM ,adPPYba,   
-  88   a8"     "8a 88P'   "88"    "8a ""     `Y8   88   a8"     "8a  
-  88   8b       d8 88      88      88 ,adPPPPP88   88   8b       d8  
-  88,  "8a,   ,a8" 88      88      88 88,    ,88   88,  "8a,   ,a8"  
-  "Y888 `"YbbdP"'  88      88      88 `"8bbdP"Y8   "Y888 `"YbbdP"'       
+  ,d                                               ,d              
+  88                                               88              
+MM88MMM ,adPPYba,  88,dPYba,,adPYba,  ,adPPYYba, MM88MMM ,adPPYba, 
+  88   a8"     "8a 88P'   "88"    "8a ""     `Y8   88   a8"     "8a
+  88   8b       d8 88      88      88 ,adPPPPP88   88   8b       d8
+  88,  "8a,   ,a8" 88      88      88 88,    ,88   88,  "8a,   ,a8"
+  "Y888 `"YbbdP"'  88      88      88 `"8bbdP"Y8   "Y888 `"YbbdP"' 
 """.strip("\n")
 
 
